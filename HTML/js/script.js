5e6bb70d31bc98715d3745278ff80041e5b1c8b3
Fernando Rodrigues <lkgfernando@gmail.com>
2026-07-17 22:56:49 -0300

    fix:feito o ajuste para cliecar nos icones e aparecer suas paginas

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,63 +1,19 @@
-console.log('Hello Word!!!')
-
-
-
-function start() {
-
-    console.log(mostraDiaNascimento('2014/02/29'))
-
-}
-
-const mostraDiaNascimento = (data) => {
-    
-    let dataNascimento = new Date(data)
-    let numeroDiaNasc  = dataNascimento.getDay()
-    let nomeDiaNasc    = ''
-
-
-    switch(numeroDiaNasc){
-        case 0:
-            nomeDiaNasc = 'Domingo'
+function showContent(conteudo){
+    switch(conteudo){
+        case 'home':
+            document.getElementById('home').className = ''
+            document.getElementById('clientes').className = 'hidden'
+            document.getElementById('orcamentos').className = 'hidden'
             break
-        case 1:
-            nomeDiaNasc = 'Segunda-Feira'
+        case 'clientes':
+            document.getElementById('home').className = 'hidden'
+            document.getElementById('clientes').className = ''
+            document.getElementById('orcamentos').className = 'hidden'
             break
-        case 2:
-            nomeDiaNasc = 'Terça-Feira'
+        case 'orcamentos':
+            document.getElementById('home').className = 'hidden'
+            document.getElementById('clientes').className = 'hidden'
+            document.getElementById('orcamentos').className = ''
             break
-        case 3:
-            nomeDiaNasc = 'Quarta-Feira'
-            break        
-        case 4:
-            nomeDiaNasc = 'Quinta-Feira'
-            break        
-        case 5:
-            nomeDiaNasc = 'Sexta-Feira'
-            break
-        case 6:
-            nomeDiaNasc = 'Sábado'
-            break
-        default:
-            isNaN(numeroDiaNasc) ? nomeDiaNasc = 'Dia não existe' : null
     }
-
-    // if(numeroDiaNasc === 0) {
-    //     nomeDiaNasc = 'Domingo'
-    // } else if(numeroDiaNasc === 1) {
-    //     nomeDiaNasc = 'Segunda-Feira'
-    // } else if(numeroDiaNasc === 2) {
-    //     nomeDiaNasc = 'Terça-Feira'
-    // }else if(numeroDiaNasc === 3) {
-    //     nomeDiaNasc = 'Quarta-Feira'
-    // }else if(numeroDiaNasc === 4) {
-    //     nomeDiaNasc = 'Quinta-Feira'
-    // }else if(numeroDiaNasc === 5) {
-    //     nomeDiaNasc = 'Sexta-Feira'
-    // }else {
-    //     nomeDiaNasc = 'Sabádo'
-    // }
-
-    return nomeDiaNasc
 }
-
-start()
